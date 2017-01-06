f9ee4ca39ff2d93c00434acdb237f26527ad4fae
dnp87 <daniil.polyanskiy@gmail.com>
2017-01-06 21:46:43 +0300

    Table layout tryouts

--- IgoNudger/IgoNudger.Droid/Resources/Resource.designer.cs
+++ IgoNudger/IgoNudger.Droid/Resources/Resource.designer.cs
@@ -60,14 +60,14 @@ namespace IgoNudger.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000a
-			public const int Check1 = 2131034122;
-			
-			// aapt resource value: 0x7f050008
-			public const int Text1 = 2131034120;
+			// aapt resource value: 0x7f05000c
+			public const int Check1 = 2131034124;
 			
 			// aapt resource value: 0x7f050009
-			public const int Text2 = 2131034121;
+			public const int Text1 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int Text2 = 2131034122;
 			
 			// aapt resource value: 0x7f050000
 			public const int addTaskButton = 2131034112;
@@ -78,14 +78,20 @@ namespace IgoNudger.Droid
 			// aapt resource value: 0x7f050004
 			public const int descriptionEdit = 2131034116;
 			
-			// aapt resource value: 0x7f050007
-			public const int linearLayout1 = 2131034119;
-			
 			// aapt resource value: 0x7f050003
 			public const int nameEdit = 2131034115;
 			
 			// aapt resource value: 0x7f050006
 			public const int saveButton = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int tableLayout1 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int tableRow1 = 2131034120;
+			
+			// aapt resource value: 0x7f05000b
+			public const int tableRow2 = 2131034123;
 			
 			// aapt resource value: 0x7f050002
 			public const int taskList = 2131034114;
